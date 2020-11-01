51d6354edbc4327931eeb357a8d7b49312b993a1
Leon De Silva <leosoft.l007@gmail.com>
2020-11-01 11:56:06 +0530

    Changes to support responsiveness.

--- js/add-card.js
+++ js/add-card.js
@@ -1,11 +1,11 @@
 var card_id = 0;
 
 function addCard1(heading, btnText, funcName) {
-    var myCol = $(`<div class="col-sm-4" id="mycol"></div>`);
+    var myCol = $(`<div class="col-lg-4 col-md-6" id="mycol"></div>`);
     var myPanel = $(`<div class="card" id=util-card-"` + card_id + `"style="margin-top: 1rem; width: 24rem;">
       <div class="card-body">
         <h5 class="card-title">`  + heading + `</h5>
-        <textarea readonly style="height: 5rem; width: 20rem;"></textarea>
+        <textarea readonly style="height: 5rem; width: 100%;"></textarea>
         <a href="#" class="btn btn-primary">` + btnText+ `</a>
         <a href="#" class="btn close">Close</a>
       </div>
@@ -22,12 +22,12 @@ function addCard1(heading, btnText, funcName) {
 }
 
 function addCard2(heading, btnText, funcName) {
-    var myCol = $(`<div class="col-sm-4" id="mycol"></div>`);
+    var myCol = $(`<div class="col-lg-4 col-md-6" id="mycol"></div>`);
     var myPanel = $(`<div class="card" id=util-card-"` + card_id + `"style="margin-top: 1rem; width: 24rem;">
       <div class="card-body">
         <h5 class="card-title">`  + heading + `</h5>
-        <textarea placeholder="Input" style="height: 5rem; width: 20rem;"></textarea>
-        <textarea readonly style="height: 5rem; width: 20rem;"></textarea>
+        <textarea placeholder="Input" style="height: 5rem; width: 100%;"></textarea>
+        <textarea readonly style="height: 5rem; width: 100%;"></textarea>
         <a href="#" class="btn btn-primary">` + btnText+ `</a>
         <a href="#" class="btn close">Close</a>
       </div>
