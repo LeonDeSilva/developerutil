5573c168cbd930799f1dd26962ae6d1a0620abfa
Leon De Silva <leosoft.l007@gmail.com>
2020-11-01 20:47:58 +0530

    Added styling for cards.

--- js/add-card.js
+++ js/add-card.js
@@ -3,12 +3,14 @@ var card_id = 0;
 function addCard1(heading, btnText, funcName) {
     card_id++; 
     var myCol = $(`<div class="col-lg-4 col-md-6" id="mycol"></div>`);
-    var myPanel = $(`<div class="card" id="util-card-` + card_id + `" style="margin-top: 1rem; width: 24rem;">
+    var myPanel = $(`<div class="card" id="util-card-` + card_id + `" style="margin-top: 1rem; width: 105%;">
       <div class="card-body">
-        <h5 class="card-title">`  + heading + `</h5>
+        <h5 class="card-title" style="float:left;">`  + heading + `</h5>
+        <button type="button" class="close" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
         <textarea readonly style="height: 5rem; width: 100%;" id="text-area-` + card_id + `"></textarea>
         <a href="#" class="btn btn-primary" onclick="` + funcName + `('` + card_id + `')">` + btnText+ `</a>
-        <a href="#" class="btn close">Close</a>
       </div>
     </div>`);
 
@@ -25,13 +27,15 @@ function addCard1(heading, btnText, funcName) {
 function addCard2(heading, btnText, funcName) {
     card_id++; 
     var myCol = $(`<div class="col-lg-4 col-md-6" id="mycol"></div>`);
-    var myPanel = $(`<div class="card" id="util-card-` + card_id + `" style="margin-top: 1rem; width: 24rem;">
+    var myPanel = $(`<div class="card" id="util-card-` + card_id + `" style="margin-top: 1rem; width: 105%;">
       <div class="card-body">
-        <h5 class="card-title">`  + heading + `</h5>
+        <h5 class="card-title" style="float:left;">`  + heading + `</h5>
+        <button type="button" class="close" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
         <textarea placeholder="Input" style="height: 5rem; width: 100%;" id="input-text-area-` + card_id + `"></textarea>
         <textarea readonly style="height: 5rem; width: 100%;" id="text-area-` + card_id + `"></textarea>
         <a href="#" class="btn btn-primary" onclick="` + funcName + `('` + card_id + `')">` + btnText+ `</a>
-        <a href="#" class="btn close">Close</a>
       </div>
     </div>`);
 
