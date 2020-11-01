53c77c96c3d6fa056dc84cd1a7fcbe167091955e
Leon De Silva <leosoft.l007@gmail.com>
2020-11-01 22:52:54 +0530

    Adde hex encoding and decoding support.

--- js/util-functions.js
+++ js/util-functions.js
@@ -11,6 +11,24 @@ function fixedEncodeURIComponent(str) {
     })
 }
 
+function toHex(s) {
+    // utf8 to latin1
+    var s = unescape(encodeURIComponent(s))
+    var h = ''
+    for (var i = 0; i < s.length; i++) {
+        h += s.charCodeAt(i).toString(16)
+    }
+    return h
+}
+
+function fromHex(h) {
+    var s = ''
+    for (var i = 0; i < h.length; i+=2) {
+        s += String.fromCharCode(parseInt(h.substr(i, 2), 16))
+    }
+    return decodeURIComponent(escape(s))
+}
+
 // Util Functions
 function generateGuid(i) {
     var x = generateGuid2();
@@ -38,5 +56,17 @@ function encodeUrl(i) {
 function decodeUrl(i) {
     var input = document.getElementById("input-text-area-" + i).value;
     var x = decodeURIComponent(input);
+    document.getElementById("text-area-" + i).textContent = x;
+}
+
+function encodeHex(i) {
+    var input = document.getElementById("input-text-area-" + i).value;
+    var x = toHex(input);
+    document.getElementById("text-area-" + i).textContent = x;
+}
+
+function decodeHex(i) {
+    var input = document.getElementById("input-text-area-" + i).value;
+    var x = fromHex(input);
     document.getElementById("text-area-" + i).textContent = x;
 }
